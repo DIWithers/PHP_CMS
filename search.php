1510317d9cb2c086dd89a585c50883fb95daca31
DanielleIvy <d.ivy.withers@gmail.com>
2018-05-09 13:09:25 -0400

    Can now search blog by tags via search sidebar

--- search.php
+++ search.php
@@ -4,19 +4,28 @@
       <div class="container">
         <div class="row">
             <div class="col-md-8">
-
-                 <?php 
-                    $query = "SELECT * FROM posts";
-                    $select_all_posts_query = mysqli_query($connection, $query);
-                    while($row = mysqli_fetch_assoc($select_all_posts_query)) {
-                        $post_title = $row['post_title'];
-                        $post_author = $row['post_author'];
-                        $post_date = $row['post_date'];
-                        $post_image = $row['post_image'];
-                        $post_content = $row['post_content'];
-                 ?>
-
-                <h1 class="page-header">
+            <?php 
+                if(isset($_POST['submit'])) {
+                    $search = $_POST['search'];
+                    $query = "SELECT * FROM posts WHERE post_tags LIKE '%$search%'";
+                    $search_query = mysqli_query($connection, $query);
+                    if (!$search_query) {
+                        die("QUERY FAILED: " . mysqli_error($connection));
+                    }
+                    $count = mysqli_num_rows($search_query);
+                    if ($count == 0) {
+                        echo "<h1 class='text-danger'> No search results </h1>";
+                    }
+                    else {
+                        while($row = mysqli_fetch_assoc($search_query)) {
+                            $post_title = $row['post_title'];
+                            $post_author = $row['post_author'];
+                            $post_date = $row['post_date'];
+                            $post_image = $row['post_image'];
+                            $post_content = $row['post_content'];
+                         }
+                         ?>
+               <h1 class="page-header">
                     Page Heading
                     <small>Secondary Text</small>
                 </h1>
@@ -32,18 +41,20 @@
                 <hr>
                 <p><?php echo $post_content ?></p>
                 <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-                 <hr>                        
-                   <?php 
-                 } ?>
-
-                <ul class="pager">
+                 <hr>    
+                 <ul class="pager">
                     <li class="previous">
                         <a href="#">&larr; Older</a>
                     </li>
                     <li class="next">
                         <a href="#">Newer &rarr;</a>
                     </li>
-                </ul>
+                </ul>                    
+                   <?php 
+                 } ?>
+              <?php }
+            ?>
+ 
 
             </div>
             <?php include "includes/sidebar.php"; ?>
