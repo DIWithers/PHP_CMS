dc264a2f8c4feacaf36c267f0d0d93147f96be49
DanielleIvy <d.ivy.withers@gmail.com>
2018-05-29 11:14:45 -0400

    relate categories to posts; protect against unnamed categories

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -21,17 +21,34 @@
                                         $post_id = $row['post_id'];
                                         $post_author = $row['post_author'];
                                         $post_title = $row['post_title'];
-                                        $post_cat = $row['post_category_id'];
+                                        $post_category_id = $row['post_category_id'];
                                         $post_status = $row['post_status'];
                                         $post_image = $row['post_image'];
                                         $post_tags = $row['post_tags'];
                                         $post_comment_count = $row['post_comment_count'];
                                         $post_date = $row['post_date'];  
+
+                                        $query = "SELECT * FROM categories WHERE cat_id = {$post_category_id}";
+                                        $select_categories_id = mysqli_query($connection, $query);
+                                        $num_categories = mysqli_num_rows($select_categories_id);
+
+
                                         echo " <tr>
                                                     <td>{$post_id}</td>
                                                     <td>{$post_author}</td>
-                                                    <td>{$post_title}</td>
-                                                    <td>{$post_cat}</td>
+                                                    <td>{$post_title}</td> ";
+                                                    if ($num_categories > 0) {
+                                                    while($row = mysqli_fetch_assoc($select_categories_id)) {
+                                                        
+                                                            $cat_id = $row['cat_id'];
+                                                            $cat_title = $row['cat_title'];
+                                                        } 
+                                                    }
+                                                    else {
+                                                        $cat_id = "None";
+                                                    }
+                                              echo "<td>{$cat_id}</td>";
+                                        echo "
                                                     <td>{$post_status}</td>
                                                     <td><img class='post-thumbnail' src='../images/{$post_image}' alt='post image'></td>
                                                     <td>{$post_tags}</td>
