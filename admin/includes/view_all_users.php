659018ccd8ad8dfc34eb828645de101d10fb04e4
DanielleIvy <d.ivy.withers@gmail.com>
2018-05-31 19:40:34 -0400

    display users

--- admin/includes/view_all_users.php
+++ admin/includes/view_all_users.php
@@ -2,56 +2,38 @@
                           <thead>
                               <tr>
                                   <th>Id</th>
-                                  <th>Author</th>
-                                  <th>Comment</th>
+                                  <th>Username</th>
+                                  <th>Firstname</th>
+                                  <th>Lastname</th>
                                   <th>Email</th>
-                                  <th>Status</th>
-                                  <th>In Response To</th>
-                                  <th>Date</th>
-                                  <th>Approve</th>
-                                  <th>Unapprove</th>
-                                  <th>Delete</th>
+                                  <th>Role</th>
                               </tr>
                           </thead>
                             <tbody>
                                 <?php
                                     global $connection;
-                                    $query = "SELECT * FROM comments";
-                                    $select_comments = mysqli_query($connection, $query);
+                                    $query = "SELECT * FROM users";
+                                    $select_users = mysqli_query($connection, $query);
 
-                                    while($row = mysqli_fetch_assoc($select_comments)) {
-                                        $comment_id = $row['comment_id'];
-                                        $comment_post_id = $row['comment_post_id'];
-                                        $comment_author = $row['comment_author'];
-                                        $comment_email = $row['comment_email'];
-                                        $comment_content = $row['comment_content'];
-                                        $comment_status = $row['comment_status'];
-                                        $comment_date = $row['comment_date'];
+                                    while($row = mysqli_fetch_assoc($select_users)) {
+                                        $user_id = $row['user_id'];
+                                        $username = $row['username'];
+                                        $user_firstname = $row['user_firstname'];
+                                        $user_lastname = $row['user_lastname'];
+                                        $user_email = $row['user_email'];
+                                        $user_image = $row['user_image'];
+                                        $user_role = $row['user_role'];
+                                        $user_password = $row['user_password'];
 
                                         echo " <tr>
-                                                    <td>{$comment_id}</td>
-                                                    <td>{$comment_author}</td>
-                                                    <td>{$comment_content}</td> ";
-
-                                        echo "
-                                                    <td>{$comment_email}</td>
-                                                    <td>{$comment_status}</td> ";
-                                                    
-                                                    $query = "SELECT * FROM posts WHERE post_id = $comment_post_id";
-                                                    $original_post = mysqli_query($connection, $query);
-                                                    while($row = mysqli_fetch_assoc($original_post)) {
-                                                    $post_id =  $row['post_id'];
-                                                    $post_title =  $row['post_title'];
-                                            
-                                        }
-                                        echo "
-                                                    <td><a href='../post.php?p_id=$post_id'</a>{$post_title}</td>
-
-                                                    <td>{$comment_date}</td>
-                                                    <td><a href='comments.php?approve=$comment_id'>Approve</a></td>
-                                                    <td><a href='comments.php?unapprove=$comment_id'>Unapprove</a></td>
-                                                    <td><a href='comments.php?delete={$comment_id}'>Delete</a></td>
-                                                </tr> ";    
+                                                    <td>{$user_id}</td>
+                                                    <td>{$user_name}</td>
+                                                    <td>{$user_firstname}</td>
+                                                    <td>{$user_lastname}</td>
+                                                    <td>{$user_email}</td>
+                                                    <td>{$user_role}</td>
+                                                </tr>
+                                             ";
                                     }
                                 ?>   
                             </tbody>
