65a4b26b40b0202e194f7a4ce0a0811b293e16f5
DanielleIvy <d.ivy.withers@gmail.com>
2018-05-31 23:03:37 -0400

    populate category with previously selected category

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -65,7 +65,8 @@
                         while($row = mysqli_fetch_assoc($select_categories)) {
                             $cat_id = $row['cat_id'];
                             $cat_title = $row['cat_title'];
-                            echo "<option value='{$cat_id}'>{$cat_title}</option>";
+                            if ($post_cat === $cat_id) echo "<option value='{$cat_id}' selected>{$cat_title}</option>";
+                            else echo "<option value='{$cat_id}'>{$cat_title}</option>";
                         }
                 ?>
             </select>
