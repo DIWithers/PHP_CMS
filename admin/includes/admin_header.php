4e29a0dfed52458b2af373023cfe2630d345fc18
DanielleIvy <d.ivy.withers@gmail.com>
2018-06-01 13:47:42 -0400

    logout feature

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -1,9 +1,8 @@
 <?php ob_start(); ?>
 <?php session_start(); ?>
 <?php 
-    if (isset($_SESSION['user_role']) && $_SESSION['user_role'] !== 'Admin' ) {
-        header("Location: ../index.php");
-    }
+    if (!isset($_SESSION['user_role'])) header("Location: ../index.php");
+    if (isset($_SESSION['user_role']) && $_SESSION['user_role'] !== 'Admin' ) header("Location: ../index.php");
 ?>
 
 <?php include "../includes/db.php" ?>
