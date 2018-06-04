a116deca47c1cac381ea289dd28b4ec9c40aa5ad
DanielleIvy <d.ivy.withers@gmail.com>
2018-06-04 15:30:04 -0400

    registration form

--- registration.php
+++ registration.php
@@ -1,6 +1,31 @@
 <?php  include "includes/db.php"; ?>
 <?php  include "includes/header.php"; ?>
 
+<?php 
+    if (isset($_POST['submit'])) {
+        if (!empty($username) && !empty($email) && !empty($password)) {
+            $username = $_POST['username'];
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+    
+            $username = mysqli_real_escape_string($connection, $username);
+            $email = mysqli_real_escape_string($connection, $email);
+            $password = mysqli_real_escape_string($connection, $password);
+    
+            $query = "SELECT randSalt FROM users";
+            $select_randsalt_query = mysqli_query($connection, $query);
+            if (!$select_randsalt_query) die("QUERY FAILED: " . mysqli_error($connection)); 
+            $row = mysqli_fetch_array($select_randsalt_query);
+            $salt = $row['randSalt'];
+
+            $query = "INSERT INTO users (username, user_email, user_password, user_role) ";
+            $query .= "VALUES('{$username}', '{$email}', '{$password}', 'subscriber')";
+            $register_user_query = mysqli_query($connection, $query);
+            if (!$register_user_query) die("QUERY FAILED: " . mysqli_error($connection)); 
+        }
+    }
+?>
+
 <div class="container">
     <section id="login">
         <div class="container">
@@ -11,15 +36,15 @@
                         <form role="form" action="registration.php" method="post" id="login-form" autocomplete="off">
                             <div class="form-group">
                                 <label for="username" class="sr-only">username</label>
-                                <input type="text" name="username" id="username" class="form-control" placeholder="Enter Desired Username">
+                                <input type="text" name="username" id="username" class="form-control" placeholder="Enter Desired Username" required>
                             </div>
                             <div class="form-group">
                                 <label for="email" class="sr-only">Email</label>
-                                <input type="email" name="email" id="email" class="form-control" placeholder="somebody@example.com">
+                                <input type="email" name="email" id="email" class="form-control" placeholder="somebody@example.com" required>
                             </div>
                             <div class="form-group">
                                 <label for="password" class="sr-only">Password</label>
-                                <input type="password" name="password" id="key" class="form-control" placeholder="Password">
+                                <input type="password" name="password" id="key" class="form-control" placeholder="Password" required>
                             </div>
                             <input type="submit" name="submit" id="btn-login" class="btn btn-custom btn-lg btn-block" value="Register">
                         </form>
