d8a37db22b2e77ff110a9160cf5258702cc9ee6f
DanielleIvy <d.ivy.withers@gmail.com>
2018-06-14 08:56:34 -0400

    escape apostrophes for SQL

--- admin/functions.php
+++ admin/functions.php
@@ -59,7 +59,7 @@
         $query = "DELETE FROM posts WHERE post_id =" . mysqli_real_escape_string($connection, $post_id_to_delete) . " ";
         $deletion_query = mysqli_query($connection, $query);
         header("Location: posts.php");
-        $confirmQuery($deletion_query);
+        // $confirmQuery($deletion_query);
     }
 
     function resetPostCount($post_id_to_update) {
@@ -77,12 +77,12 @@
 
         while($row = mysqli_fetch_assoc($select_posts)) {
             $post_id = $row['post_id'];
-            $post_author = $row['post_author'];
-            $post_title = $row['post_title'];
+            $post_author = str_replace("'", "''", $row['post_author']);
+            $post_title = str_replace("'", "''", $row['post_title']);
             $post_category_id = $row['post_category_id'];
             $post_status = $row['post_status'];
             $post_image = $row['post_image'];
-            $post_tags = $row['post_tags'];
+            $post_tags = str_replace("'", "''", $row['post_tags']);
             $post_comment_count = $row['post_comment_count'];
             $post_date = $row['post_date'];  
         }
